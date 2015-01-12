e322841987713c8a3dc565932eb5818364a6b06a
pbmolini <pbmolini@fbk.eu>
2015-01-12 22:47:16 +0100

    some validations and root route changed

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,3 +1,8 @@
 class Boat < ActiveRecord::Base
+
+	validates :name, :manufacturer, :daily_price, :year, :model, :length, :guest_capacity, :boat_category, presence: true
+	validates :year, numericality: { greater_than: 1900 }
+	validates :daily_price, numericality: { greater_than: 1 }
+	
   belongs_to :boat_category
 end
