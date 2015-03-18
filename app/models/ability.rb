9df15509715049183c7e4a6bf312560ad07f032a
Aaron Ciaghi <aaron.ciaghi@gmail.com>
2015-03-18 22:14:47 +0100

    added admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,15 +5,20 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     user ||= User.new # guest user (not logged in)
-    alias_action :create, :read, :update, :destroy, :to => :crud
 
-    can [:read, :create], Boat
-    can :crud, Boat, user_id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      alias_action :create, :read, :update, :destroy, :to => :crud
 
-    can :read, Booking, boat: { user_id: user.id }
+      can [:read, :create], Boat
+      can :crud, Boat, user_id: user.id
 
-    can :create, Booking do |booking|
-        booking.user == user && booking.boat.user != user
+      can :read, Booking, boat: { user_id: user.id }
+
+      can :create, Booking do |booking|
+          booking.user == user && booking.boat.user != user
+      end
     end
 
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304212610) do
+ActiveRecord::Schema.define(version: 20150318211130) do
 
   create_table "boat_categories", force: :cascade do |t|
     t.string   "name"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20150304212610) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
