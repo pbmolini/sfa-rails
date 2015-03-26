b1916596af3ffe3da97baef366bf9669975e0dd3
Aaron Ciaghi <aaron.ciaghi@gmail.com>
2015-03-26 22:45:22 +0100

    temporarily switched to deliver_now for welcome emails

--- app/observers/user_observer.rb
+++ app/observers/user_observer.rb
@@ -1,7 +1,8 @@
 class UserObserver < ActiveRecord::Observer
 
   def after_create user
-    UserMailer.welcome_email(user).deliver_later
+    UserMailer.welcome_email(user).deliver_now
+    #you should use deliver_later
   end
 
 end
