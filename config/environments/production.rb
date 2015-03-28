502fdf4866f8bba05b8342226bee5a14345a467e
pbmolini <pbmolini@fbk.eu>
2015-03-29 00:20:54 +0100

    trying to make s3 work che palle

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,8 +18,8 @@ Rails.application.configure do
   storage: :s3,
   url: ':s3_domain_url',
   path: '/:class/:attachment/:id_partition/:style/:filename',
-  s3_protocol: 'https',
-  s3_host_name: 's3-eu-west-1.amazonaws.com',
+  # s3_protocol: 'https',
+  # s3_host_name: 's3-eu-west-1.amazonaws.com',
   s3_credentials: {
     bucket: ENV['S3_BUCKET_NAME'],
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
