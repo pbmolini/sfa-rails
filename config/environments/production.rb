7398c70d6e065f85d6f880a2add383ff1f50b5cd
pbmolini <pbmolini@fbk.eu>
2015-03-29 00:15:26 +0100

    trying to make s3 work again hope no more

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,14 +15,15 @@ Rails.application.configure do
 
   # Paperclip AWS S3
   config.paperclip_defaults = {
-  :storage => :s3,
-  url: 'sfa-staging.s3.amazonaws.com',
+  storage: :s3,
+  url: ':s3_domain_url',
   path: '/:class/:attachment/:id_partition/:style/:filename',
-  s3_host_name: 'eu-central-1.amazonaws.com',
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  s3_protocol: 'https',
+  s3_host_name: 's3-eu-west-1.amazonaws.com',
+  s3_credentials: {
+    bucket: ENV['S3_BUCKET_NAME'],
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
   # Code is not reloaded between requests.
