03c491a3497d05ebb8d4ddd0a43eb328c10792b7
pbmolini <pbmolini@fbk.eu>
2015-03-28 23:34:56 +0100

    trying to make s3 work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,8 @@ Rails.application.configure do
   # Paperclip AWS S3
   config.paperclip_defaults = {
   :storage => :s3,
+  url: 'sfa-staging.s3.amazonaws.com',
+  path: '/:class/:attachment/:id_partition/:style/:filename',
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
