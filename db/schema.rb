c095ab309210a6823f47b2d7238c926087266d89
Aaron Ciaghi <aaron.ciaghi@gmail.com>
2015-04-01 22:52:05 +0200

    added facebook login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326220447) do
+ActiveRecord::Schema.define(version: 20150401193532) do
 
   create_table "boat_categories", force: :cascade do |t|
     t.string   "name"
@@ -84,6 +84,8 @@ ActiveRecord::Schema.define(version: 20150326220447) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
