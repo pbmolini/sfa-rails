3ad7f341e6e04152c397335cc96d6b98555484b2
Aaron Ciaghi <aaron.ciaghi@gmail.com>
2015-06-10 23:17:12 +0200

    added validation when deleting pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,4 +4,12 @@ class Picture < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_attachment_size :image, less_than: 10.megabytes
+
+  before_destroy :check_pictures_count
+
+  private
+
+  def check_pictures_count
+    boat.pictures.count > 1
+  end
 end
