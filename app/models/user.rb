9bfcf80136c0e4e72f0606a7662483c2762dd248
pbmolini <pbmolini@fbk.eu>
2015-07-28 14:42:10 +0200

    user avatar can now be bigger. user default pic (guybrush) is now small in user/edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   has_many :boats
   has_many :bookings
 
-  has_attached_file :image, :styles => { :thumb => "50x50#" }, default_url: "default_avatar.png"
+  has_attached_file :image, :styles => { thumb: "50x50#", medium: "150x150#" }, default_url: "default_avatar.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_presence_of :first_name, :last_name
   validates :birthdate, presence: true, on: :update
