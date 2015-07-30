af6128ed1a3386fab4aef130398ae0249c1e49a5
pbmolini <pbmolini@fbk.eu>
2015-07-30 18:02:06 +0200

    boat/show looks better when the boat is incomplete

--- app/helpers/boats_helper.rb
+++ app/helpers/boats_helper.rb
@@ -7,4 +7,34 @@ module BoatsHelper
       end
     end
   end
+
+  def text_for boat, field_name
+  	if boat.send(field_name.to_s).blank?
+  		content_tag :span, _("N/A"), class: 'incomplete-field'
+  	else
+  		boat.send(field_name.to_s)
+  	end
+  end
+
+  def placeholder_url_for location
+		height = '320'
+		width = '480'
+		text_size	= '100'
+		text_color = 'fab647'
+		background = 'd3d3d3'
+		text = CGI.escape _("Edit to add\npictures\n ")
+
+  	case location
+  	when :boat_card
+  		height = '250'
+  		width = '300'
+			text_size	= '42'
+  	when :boat_show
+  		height = '600'
+  		width = '800'
+			text_size	= '120'
+  	end
+  	
+  	"//placeholdit.imgix.net/~text?txtsize=#{text_size}&txt=#{text}&w=#{width}&h=#{height}&bg=#{background}&txtclr=#{text_color}"
+  end
 end
