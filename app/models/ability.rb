670c493ab1e4a49c856b1aaecaa7ea9218d8db0b
pbmolini <pbmolini@fbk.eu>
2015-08-28 17:21:10 +0200

    removed admin column from users table

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,33 +7,29 @@ class Ability
     case user
     when AdminUser
       can :manage, :all
+      
     when User
+    user ||= User.new # guest user (not logged in)
 
-      user ||= User.new # guest user (not logged in)
+      alias_action :create, :read, :update, :destroy, :to => :crud
 
-      if user.admin?
-        can :manage, :all
-        # da cavare
-      else
-        alias_action :create, :read, :update, :destroy, :to => :crud
+      can [:read, :create], Boat
+      can [:crud, :publish], Boat, user_id: user.id
 
-        can [:read, :create], Boat
-        can [:crud, :publish], Boat, user_id: user.id
-
-        can [:read, :create], BoatFeaturesSet
-        can :crud, BoatFeaturesSet do |bfs|
-          bfs.boat.user == user
-        end
-        can :crud, Picture do |pic|
-          pic.boat.user == user
-        end
-
-        can :read, Booking, boat: { user_id: user.id }
-
-        can :create, Booking do |booking|
-            booking.user == user && booking.boat.user != user
-        end
+      can [:read, :create], BoatFeaturesSet
+      can :crud, BoatFeaturesSet do |bfs|
+        bfs.boat.user == user
       end
+      can :crud, Picture do |pic|
+        pic.boat.user == user
+      end
+
+      can :read, Booking, boat: { user_id: user.id }
+
+      can :create, Booking do |booking|
+          booking.user == user && booking.boat.user != user
+      end
+
     end
 
   end
