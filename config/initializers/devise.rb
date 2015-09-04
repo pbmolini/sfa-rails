34d5319c00c5d08aba9865f2e26120bef7a5e705
pbmolini <pbmolini@fbk.eu>
2015-09-04 22:15:29 +0200

    independent logout for devise users

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -213,7 +213,8 @@ Devise.setup do |config|
 
   # Set this configuration to false if you want /users/sign_out to sign out
   # only the current scope. By default, Devise signs out all scopes.
-  # config.sign_out_all_scopes = true
+  # In questo modo :admin e :user fanno logout in maniera indipendente 
+  config.sign_out_all_scopes = false
 
   # ==> Navigation configuration
   # Lists the formats that should be treated as navigational. Formats like
