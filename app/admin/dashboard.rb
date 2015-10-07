1c55cd35652e7217f0598501b88600086fdc5224
pbmolini <pbmolini@fbk.eu>
2015-10-07 22:26:48 +0200

    a nicer admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -6,20 +6,35 @@ ActiveAdmin.register_page "Dashboard" do
     
     columns do
       column do
-        panel "Recent Boats" do
+        panel "Benvenuto!" do
+          h1 "Questa è l'interfaccia di Amministrazione di <strong>Sea for All</strong>".html_safe
+          h2 "Ci sono #{User.count} utenti iscritti, l'ultimo si è iscritto #{time_ago_in_words(User.last.created_at)} fa"
+          h2 "Le barche iscritte sono #{Boat.count}, di cui #{Boat.complete.count} completate"
+        end
+      end
+    end
+
+    columns do
+      column do
+        panel "Gli ultimi 5 utenti iscritti" do
           ul do
-            Boat.last(5).map do |boat|
-              li link_to(boat.name, admin_boat_path(boat))
+            User.last(5).reverse.map do |user|
+              li "#{link_to(user.name, admin_user_path(user))}, iscritto #{time_ago_in_words(user.created_at)} fa".html_safe
             end
           end
         end
       end
 
       column do
-        panel "Info" do
-          para "Welcome to ActiveAdmin."
+        panel "Le ultime 5 barche completate" do
+          ul do
+            Boat.complete.last(5).reverse.map do |boat|
+              li "#{link_to(boat.name, admin_boat_path(boat))}, di #{link_to(boat.user.name, admin_user_path(boat.user))}".html_safe
+            end
+          end
         end
       end
     end
+
   end # content
 end
