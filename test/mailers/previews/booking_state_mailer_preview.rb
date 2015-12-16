6a9076ee9ed11e50e81c285c5e26e7a9a8ab7c14
pbmolini <pbmolini@fbk.eu>
2015-12-16 22:53:48 +0100

    more text in booking_state_mailers

--- test/mailers/previews/booking_state_mailer_preview.rb
+++ test/mailers/previews/booking_state_mailer_preview.rb
@@ -1,14 +1,29 @@
 class BookingStateMailerPreview < ActionMailer::Preview
 
-	def booking_accepted
-		BookingStateMailer.booking_accepted(User.first, Booking.last, "it")
+	# Accepted
+	def booking_accepted_to_guest
+		BookingStateMailer.booking_accepted(Booking.last.user, Booking.last, "it")
 	end
 
-	def booking_rejected
-		BookingStateMailer.booking_rejected(User.first, Booking.last, "it")
+	def booking_accepted_to_host
+		BookingStateMailer.booking_accepted(Booking.last.boat.user, Booking.last, "it")
 	end
 
-	def booking_canceled
-		BookingStateMailer.booking_canceled(User.first, Booking.last, "it")
+	# Rejected
+	def booking_rejected_to_guest
+		BookingStateMailer.booking_rejected(Booking.last.user, Booking.last, "it")
+	end
+
+	def booking_rejected_to_host
+		BookingStateMailer.booking_rejected(Booking.last.boat.user, Booking.last, "it")
+	end
+
+	# Canceled
+	def booking_canceled_to_guest
+		BookingStateMailer.booking_canceled(Booking.last.user, Booking.last, "it")
+	end
+
+	def booking_canceled_to_host
+		BookingStateMailer.booking_canceled(Booking.last.boat.user, Booking.last, "it")
 	end
 end
