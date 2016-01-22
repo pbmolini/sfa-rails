db663792c02ae4f55da2b40faca93f110cd56caa
pbmolini <pbmolini@fbk.eu>
2016-01-22 11:49:46 +0100

    custom authorization on dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,19 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
+  before_action :authorize_page # This is NOT CanCan
 
   layout :layout_for_page
 
   private
+
+  # Custom authorization: CanCan skips this Controller
+  def authorize_page
+    if params[:id] == 'dashboard' and current_user.nil?
+      session[:next] = request.fullpath
+      puts session[:next]
+      redirect_to new_user_session_path, alert: _("You have to log in to continue.")
+    end
+  end
 
   def layout_for_page
     case params[:id]
