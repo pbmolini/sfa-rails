1e2a1154428e5656e03089c1f9b5719d6fcbd859
pbmolini <pbmolini@fbk.eu>
2016-01-25 14:41:49 +0100

    :host is now in .env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   }
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { :host => 'www.seaforall.com' }
+  config.action_mailer.default_url_options = { host: ENV['PRODUCTION_HOSTNAME'] }
 
   # Paperclip AWS S3
   config.paperclip_defaults = {
