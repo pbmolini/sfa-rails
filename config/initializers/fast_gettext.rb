e6455dd262a671cdc652dc32842fb24214285ee1
pbmolini <pbmolini@fbk.eu>
2016-04-26 22:00:27 +0200

    before deploy

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,3 +1,3 @@
-FastGettext.add_text_domain 'app', :path => 'locale', :type => :po
+FastGettext.add_text_domain 'app', :path => 'locale', :type => :po, ignore_fuzzy: true
 FastGettext.default_available_locales = ['en','it'] #all you want to allow
 FastGettext.default_text_domain = 'app'
