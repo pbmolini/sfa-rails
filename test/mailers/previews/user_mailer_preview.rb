aa3afd2d2ea2d65b898036bc55af6728e580c1db
pbmolini <pbmolini@fbk.eu>
2016-04-28 23:02:33 +0200

    reset pw email is now nicer and localized

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,4 +2,8 @@ class UserMailerPreview < ActionMailer::Preview
 	def welcome_email
 		UserMailer.welcome_email(User.first, "it")
 	end
+
+	def reset_password_instructions
+    UserMailer.reset_password_instructions(User.first, "faketoken", {locale: "it"})
+  end
 end
